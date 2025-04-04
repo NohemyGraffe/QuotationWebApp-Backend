b3572ba7ad88db5b7140108bf5b238c204c4569d
multyviajes <multyviajes@gmail.com>
2025-04-04 17:02:30 -0400

    Create TransportationEjeCafetero.js

---
+++ models/TransportationEjeCafetero.js
@@ -0,0 +1,22 @@
+// models/Transportation.js
+const mongoose = require('mongoose');
+
+const TransportationSchema = new mongoose.Schema({
+  type: { type: String, default: "transportationPricingEjeCafetero" },
+  saleByPerson: [
+    {
+      minPax: Number,
+      maxPax: Number,
+      priceEach: Number
+    }
+  ],
+  costByCar: [
+    {
+      minPax: Number,
+      maxPax: Number,
+      priceCar: Number
+    }
+  ]
+});
+
+module.exports = mongoose.model('TransportationEjeCafetero', TransportationSchema);
