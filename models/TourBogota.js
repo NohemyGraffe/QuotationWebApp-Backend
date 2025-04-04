32f80ae1c948e8c15c7ea86a3451fe4b5fa54d2e
multyviajes <multyviajes@gmail.com>
2025-04-04 16:58:50 -0400

    Create TourBogota.js

---
+++ models/TourBogota.js
@@ -0,0 +1,10 @@
+// models/Tour.js
+const mongoose = require('mongoose');
+
+const TourSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  internoCop: { type: Number, required: true },
+  ventaUsd: { type: Number, required: true }
+});
+
+module.exports = mongoose.model('TourBogota', TourSchema);
