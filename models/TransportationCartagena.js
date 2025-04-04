933a3d1deac0f8df2ba75c49ce6b8f548ec9e86b
multyviajes <multyviajes@gmail.com>
2025-04-04 17:01:08 -0400

    Create TransportationCartagena.js

---
+++ models/TransportationCartagena.js
@@ -0,0 +1,22 @@
+// models/Transportation.js
+const mongoose = require('mongoose');
+
+const TransportationSchema = new mongoose.Schema({
+  type: { type: String, default: "transportationPricingCartagena" },
+  saleByPerson: [
+    {
+      minPax: Number,
+      maxPax: Number,
+      priceEach: Number
+    }
+  ],
+  costByCar: [
+    {
+      minPax: Number,
+      maxPax: Number,
+      priceCar: Number
+    }
+  ]
+});
+
+module.exports = mongoose.model('TransportationCartagena', TransportationSchema);
