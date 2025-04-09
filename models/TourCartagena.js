9dd9947caab51078c36140876f99874b41eb25e7
multyviajes <multyviajes@gmail.com>
2025-04-09 13:53:17 -0400

    Update TourCartagena.js

--- models/TourCartagena.js
+++ models/TourCartagena.js
@@ -7,4 +7,4 @@ const TourSchema = new mongoose.Schema({
   ventaUsd: { type: Number, required: true }
 });
 
-module.exports = mongoose.model('TourCartagena', TourSchema);
+module.exports = mongoose.model('TourCartagena', TourSchema, 'toursCartagena');
