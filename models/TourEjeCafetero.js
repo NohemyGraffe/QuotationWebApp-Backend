ac533a8998794130bf3daa05c4b0fbb3d1ec1bf4
multyviajes <multyviajes@gmail.com>
2025-04-09 13:34:23 -0400

    Update TourEjeCafetero.js

--- models/TourEjeCafetero.js
+++ models/TourEjeCafetero.js
@@ -1,1 +1,10 @@
+// models/TourEjeCafetero.js
+const mongoose = require('mongoose');
 
+const TourSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  internoCop: { type: Number, required: true },
+  ventaUsd: { type: Number, required: true }
+});
+
+module.exports = mongoose.model('TourEjeCafetero', TourSchema);
